c9eb9f920ad50537bb37940d8764d30b48a07af9
Peter Kong <bansuki@gmail.com>
2017-09-29 12:25:15 -0500

    got basic site and article navigation working

--- js/main.js
+++ js/main.js
@@ -1,17 +1,52 @@
 /* GLOBAL VARIABLES UP HERE */
-var frontPage = 'https://www.reddit.com/.json';
-
-
-
-
+var frontPage = 'https://www.reddit.com/';
 
 $(document).ready(function(){
 /* FUNCTION EXECUTION HERE */
-  console.log('Go forth and code!');
+  // attach event listeners to nav links
+  $('.hot-new-rising-nav').click(navToArticles);
 
+  // ajax call
+  getArticles();
 });
 
+function navToArticles(e) {
+  let target = e.target;
+  e.preventDefault();
+  getArticles(target.innerText);
+}
 
+function getArticles(page = '') {
+  $.ajax({
+    method: 'GET',
+    url: `${frontPage}${page}/.json`,
+    dataType: 'json',
+    success: onSuccess,
+    error: onError
+  })
+}
+
+function onSuccess(response) {
+  displayArticles(response.data);
+}
+
+function onError(xhr) {
+  console.log(xhr);
+}
+
+function displayArticles(data) {
+  $('.hot-new-rising-body').empty();
+  data.children.forEach(article => {
+    displayAnArticle(article.data);
+  });
+}
+
+function displayAnArticle(article) {
+  $('.hot-new-rising-body').append(`<article id="${article.id}">
+<img src="${article.thumbnail}"/>
+<a href="${article.url}">${article.title}</a>
+  </article>`)
+}
 
 /* FUNCTION DEFINITION HERE */
 /* TIP: don't forget scope! */
